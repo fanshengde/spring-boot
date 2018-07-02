9adf37922132ee90025ae208dc95a6d52d8789f6
fanshengde <fanshengde@hotmail.com>
2018-07-02 19:05:01 +0800

    添加用户功能

--- boot-shiro-demo-1-web/src/main/java/com/fsd/web/UserController.java
+++ boot-shiro-demo-1-web/src/main/java/com/fsd/web/UserController.java
@@ -1,21 +1,35 @@
 package com.fsd.web;
 
+import java.util.List;
+
 import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.fsd.entity.SysUser;
+import com.fsd.entity.repository.SysUserRepository;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
-
+	@Autowired
+	SysUserRepository sysUserRepository;
 	/**
 	 * 用户查询.
 	 * 
 	 * @return
 	 */
-	@RequestMapping("/userList")
+	@RequestMapping(value = "/userList", method = RequestMethod.GET)
 	@RequiresPermissions("userInfo:view") // 权限管理;
-	public String userInfo() {
+	public String userInfo(Model model) {
+		Iterable<SysUser> sysUsers = sysUserRepository.findAll();
+		
+		model.addAttribute("sysUsers", sysUsers);
+		
 		return "userInfo";
 	}
 
@@ -24,9 +38,17 @@ public class UserController {
 	 * 
 	 * @return
 	 */
-	@RequestMapping("/userAdd")
+	@RequestMapping(value = "/userAdd", method = RequestMethod.GET)
 	@RequiresPermissions("userInfo:add") // 权限管理;
-	public String userInfoAdd() {
+	public String userInfoAdd(@ModelAttribute(value="sysUser") SysUser sysUser, Model model) {
+		model.addAttribute("sysUser", sysUser);
+		return "userInfoAdd";
+	}
+	
+	@RequestMapping(value = "/userAdd", method = RequestMethod.POST)
+	@RequiresPermissions("userInfo:add") // 权限管理;
+	public String userInfoAdd(@ModelAttribute SysUser sysUser) {
+		sysUserRepository.save(sysUser);
 		return "userInfoAdd";
 	}
 
